8f16b4c8e7b53f69a9c49d710f21aecd5ccea5f9
yueyuefei <Administrator@windows10.microdone.cn>
2018-05-14 10:54:02 +0800

    提交

--- src/main/java/com/rockontrol/env/test/JsonToExcelTest.java
+++ src/main/java/com/rockontrol/env/test/JsonToExcelTest.java
@@ -1,12 +1,12 @@
 package com.rockontrol.env.test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONArray;
@@ -20,8 +20,13 @@ public class JsonToExcelTest {
 
 	public static void main(String[] args) {
 //		jsonToExcelUtil("http://www.aqiapp.com/dm/devdata/list?type=3&number=0&size=400&mn=170201AF000014&timeBegin=1523408400000&timeEnd=1523419200000");
-		String timeBegin = "2018-05-02 13:40";
-		String timeEnd = "2018-05-02 14:16";
+		
+		String timeBegin = "2018-05-11 10:00";
+		String timeEnd = "2018-05-11 10:36";
+		if(args.length>0) {
+			timeBegin = args[0];
+			timeEnd = args[1];
+		}
 		jsonToExcelTest(timeBegin, timeEnd);
 	}
 	
@@ -38,15 +43,28 @@ public class JsonToExcelTest {
 	}
 	
 	public static void jsonToExcelTest(String timeBegin, String timeEnd) {
-		List<String> mns = Arrays.asList("170201AF000012","170201AF000014","170201AF000020");
+		List<String> mns = new ArrayList<String>();
+		mns.add("170201AF000012");
+		mns.add("170201AF000014");
+		mns.add("170201AF000020");
 		Long stime = DateUtil.parse(timeBegin).getTime();
 		Long etime = DateUtil.parse(timeEnd).getTime();
-		String url = "http://www.aqiapp.com/dm/devdata/list?type=3&number=0&size=400&mn={}&timeBegin={}&timeEnd={}";
 		List<Map<String, Object>> array = new ArrayList<Map<String,Object>>();
 		for (String mn : mns) {
+			String url = "http://www.aqiapp.com/dm/devdata/list?type=3&number=0&size=1000&mn={}&timeBegin={}&timeEnd={}";
 			url = StrUtil.format(url,mn, stime, etime); 
 			String json = HttpUtil.get(url);
+			Console.log(url);//TODO need to detete
+			
 			JSONObject obj = new JSONObject(json); 
+			if(obj.getInt("totalPages")==0) {
+				Console.log("mn={}无相关数据！",mn);//TODO need to detete
+				continue;
+			}
+			if(obj.getInt("totalPages")!=1) {
+				Console.log("mn={}数据量太大，请调整size大小！",mn);//TODO need to detete
+				continue;
+			}
 			JSONArray list = obj.getJSONArray("content");
 			for (Object object : list) {
 				Map<String, Object> map = new LinkedHashMap<String, Object>();
@@ -64,7 +82,10 @@ public class JsonToExcelTest {
 			}
 		}
 		// 通过工具类创建writer
+//		ExcelWriter writer = ExcelUtil.getWriter(System.getProperty("user.dir")+"/数据.xlsx");
 		ExcelWriter writer = ExcelUtil.getWriter("C:/Users/Administrator/Desktop/数据.xlsx");
+		writer.setColumnWidth(0, 20);
+		writer.setColumnWidth(1, 20);
 		// 一次性写出内容，使用默认样式
 		writer.write(array);
 		// 关闭writer，释放内存
